8f849203eaf3f8549f89e025247a5935417b86f2
Pushpendra Singh thakur <thakurpsr@gmail.com>
2020-05-13 15:16:59 +0530

    ADD Sample Config file

---
+++ public-sample/config.php
@@ -0,0 +1,7 @@
+<?php	 
+	/* SYSTEM_PATH - Location of WEBE */
+	define( 'SYSTEM_PATH', '/home/developer/WorkBench/webe/' );
+	
+	/* SITE_PATH - Location of website data like templates, pages, images etc */
+	define( 'SITE_PATH', '/home/developer/WorkBench/webetest/' );
+?>
